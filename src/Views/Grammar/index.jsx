e8b9b55d8dc0717cec2bd86472e9cd16c84feb47
Jonathan Jacob <jacob.verdugo.jonathan@gmail.com>
2023-07-29 15:06:31 -0600

    se agrego inicio para componente de grammarç

--- src/Views/Grammar/index.jsx
+++ src/Views/Grammar/index.jsx
@@ -1,9 +1,64 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Box, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Typography,
+} from "@mui/material";
+import { ExpandMore } from "@mui/icons-material";
 
 const Grammar = () => {
-  const [open, setOpen] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const data = [
+    {
+      title: "Possessive adjetives",
+      keywords: "I´m,not",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ab molestias, dolorem, alias quae aspernatur culpa accusamus est pariatur similique quas consectetur eveniet illum perspiciatis blanditiis eaque iure repellat nam.",
+      examples: ["I´m from Colombia", "I´m not from Venezuela"],
+    },
+    {
+      title: "Possessive adjetives",
+      keywords: "I´m,not",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ab molestias, dolorem, alias quae aspernatur culpa accusamus est pariatur similique quas consectetur eveniet illum perspiciatis blanditiis eaque iure repellat nam.",
+      examples: ["I´m from Colombia", "I´m not from Venezuela"],
+    },
+    {
+      title: "Possessive adjetives",
+      keywords: "I´m,not",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ab molestias, dolorem, alias quae aspernatur culpa accusamus est pariatur similique quas consectetur eveniet illum perspiciatis blanditiis eaque iure repellat nam.",
+      examples: ["I´m from Colombia", "I´m not from Venezuela"],
+    },
+    {
+      title: "Possessive adjetives",
+      keywords: "I´m,not",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ab molestias, dolorem, alias quae aspernatur culpa accusamus est pariatur similique quas consectetur eveniet illum perspiciatis blanditiis eaque iure repellat nam.",
+      examples: ["I´m from Colombia", "I´m not from Venezuela"],
+    },
+  ];
+
+  const addStrong = (example, keywords) => {
+    const kewordsArray = keywords.split(",");
+    kewordsArray.forEach((keyword) => {
+      const regexp = new RegExp(keyword, "gi");
+      example = String(example).replace(
+        regexp,
+        "<strong>" + keyword + "</strong>"
+      );
+    });
+
+    return example;
+  };
   return (
     <>
       <Box
@@ -31,9 +86,34 @@ const Grammar = () => {
           GRAMMAR
         </Typography>
       </Box>
-      <Typography variant="h1" component="div">
-        COMING SOON
-      </Typography>
+
+      <Box>
+        {data.map((data, key) => (
+          <Accordion
+            key={key}
+            expanded={expanded === "panel_" + key}
+            onChange={handleChange("panel_" + key)}
+          >
+            <AccordionSummary expandIcon={<ExpandMore />} id={"panel_" + key}>
+              <Typography fontSize={18}>{data.title}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>{data.description}</Typography>
+              <ul>
+                {data.examples.map((example, key2) => (
+                  <li key={key2}>
+                    <Typography
+                      dangerouslySetInnerHTML={{
+                        __html: addStrong(example, data.keywords),
+                      }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </Box>
     </>
   );
 };
